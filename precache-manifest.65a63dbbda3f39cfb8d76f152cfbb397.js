3c3e9014dbd2f757d818432dda2470ce5605d131
Ted <truongnm97@icloud.com>
2020-05-02 10:20:46 +0700

    Updates

---
+++ precache-manifest.65a63dbbda3f39cfb8d76f152cfbb397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6061bf8bbd9b6ecae05b551951e82211",
+    "url": "/ted-portfolio/index.html"
+  },
+  {
+    "revision": "d487d9a3ad57edd9280d",
+    "url": "/ted-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a76753329ceb9e498a89",
+    "url": "/ted-portfolio/static/js/2.49a86c5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ted-portfolio/static/js/2.49a86c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d487d9a3ad57edd9280d",
+    "url": "/ted-portfolio/static/js/main.1b0b9f27.chunk.js"
+  },
+  {
+    "revision": "36f5f96cb92922566d64",
+    "url": "/ted-portfolio/static/js/runtime-main.40e9df96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ted-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
