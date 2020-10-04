116baf4279cb63041beb2062c170e7865b9b02ff
Nguyễn Minh Trường <truong.nguyen@techx.vn>
2020-10-04 18:15:27 +0700

    Update front page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import { Planet, File } from 'react-kawaii'
+import { Typography, Layout, Space, Button } from 'antd'
+import './App.scss'
+import 'antd/dist/antd.dark.css'
+
+const { Paragraph, Text } = Typography
+const { Content } = Layout
 
 function App() {
+  const [clicked, setClicked] = useState(false)
+
+  let timeout = null
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [timeout])
+
+  const onClickMe = () => {
+    setClicked(true)
+    timeout = setTimeout(() => {
+      setClicked(undefined)
+    }, 5000)
+  }
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Welcome to my honeypot</p>
-      </header>
-    </div>
+    <Layout className='app'>
+      <Content className='header'>
+        {clicked == null ? (
+          <Space size={20} direction='horizontal'>
+            <Planet size={900} mood='happy' color='#ffffff' />
+            <Text strong>NOW GTFO!!!!</Text>
+          </Space>
+        ) : clicked === true ? (
+          <Space size={10} direction='vertical'>
+            <File />
+            <Paragraph>
+              Nothing here <Text strong>YET!!!!</Text>
+            </Paragraph>
+          </Space>
+        ) : (
+          <Space size={10} direction='vertical'>
+            <div className='logo'>
+              <Planet />
+            </div>
+            <Paragraph>
+              Welcome to
+              <br />
+              <Text className='title' strong>
+                The Amazing World of Teddy
+              </Text>
+            </Paragraph>
+            <Button shape='round' size='large' onClick={onClickMe}>
+              Click me
+            </Button>
+          </Space>
+        )}
+      </Content>
+    </Layout>
   )
 }
 
